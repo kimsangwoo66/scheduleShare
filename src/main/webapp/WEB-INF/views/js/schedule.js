8a20716e0c3baaf15a9271733c120334091318ef
Soiiii <cojomkr33@gmail.com>
2023-03-04 16:12:35 +0900

    jsp

---
+++ src/main/webapp/WEB-INF/views/js/schedule.js
@@ -0,0 +1,26 @@
+let index={
+init:function(){
+$("#btn-save").on("click", () =>{
+    this.save();)
+});
+},
+
+save: function(){
+    let data = {
+        title:$("#title").val(),
+        content:$('#content').val()
+    };
+    $.ajax({
+    type:"POST",
+    url:"/api/schedule",
+    data:JSON.stringify(data),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json"
+    }).done(function(resp){
+        alert("완료되었습니다");
+        location.href= "/";
+    }).fail(function(error){
+        alert(JSON.stringify(error));
+    });
+  },
+}
