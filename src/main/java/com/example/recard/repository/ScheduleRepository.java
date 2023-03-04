2522f53f810d425abed9c5d1a077ff4a3388812d
Soiiii <cojomkr33@gmail.com>
2023-03-04 16:12:54 +0900

    re

---
+++ src/main/java/com/example/recard/repository/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.example.recard.repository;
+
+import com.example.recard.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+
+}
