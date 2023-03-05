aefd7eb5ceb6444c7ab147a2dbce49070e986167
Soiiii <cojomkr33@gmail.com>
2023-03-05 18:03:50 +0900

    좋아요 테이블 엔티티 create

--- src/main/java/com/example/recard/domain/UserLike.java
+++ src/main/java/com/example/recard/domain/UserLike.java
@@ -6,21 +6,22 @@ import javax.persistence.*;
 
 @Data
 @Entity
-public class Like {
+public class UserLike {
 
     //기본키 직접 할당해야함
     // Member member = new Member();
     //member.setId("1"); // 기본키 직접 할당
     //EntityManager.persist(member);
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer like_id;
 
     @ManyToOne
-    @JoinColumn(name="schedule_id")
+    @JoinColumn(name= "schedule_id")
     private Schedule schedule_id;
 
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user_id;
 
 }
