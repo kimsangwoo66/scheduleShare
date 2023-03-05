f2e4b4d3d4eb96f83e3edb0fe23b6d47431cc898
Soiiii <cojomkr33@gmail.com>
2023-03-05 19:03:29 +0900

    UserLike domain create

---
+++ src/main/java/com/example/recard/domain/UserLike.java
@@ -0,0 +1,27 @@
+package com.example.recard.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class UserLike {
+
+    //기본키 직접 할당해야함
+    // Member member = new Member();
+    //member.setId("1"); // 기본키 직접 할당
+    //EntityManager.persist(member);
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer like_id;
+
+    @ManyToOne
+    @JoinColumn(name= "schedule_id")
+    private Schedule schedule_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
+}
